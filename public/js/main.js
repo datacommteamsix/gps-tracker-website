c716fba624197026d5b714a6e270a7d8d87dec93
Benny Wang <bennywang75@hotmail.com>
2018-04-01 21:33:02 -0700

    Finished the website

--- public/js/main.js
+++ public/js/main.js
@@ -1,51 +1,45 @@
+// Default location for the map
 const VANCOUVER = { lat: 49.2827, lng: -123.1207 };
 
-function createCard(deviceHash, line)
-{
-  let points = "";
-  points += "<table class='table'>" +
-              "<thead>" +
-                "<tr>" +
-                  "<th scope='col'>Latitude</th>" +
-                  "<th scope='col'>Longitude</th>" +
-                "</tr>" +
-              "</thead>" +
-              "<tbody>";
+// Google Maps object
+let map = {};
 
-  line.forEach(function(point) {
-    points += "<tr>";
-    points += "<td>" + point.lat + "</td>"
-    points += "<td>" + point.lng + "</td>"
-    points += "</tr>"; 
-  });
-  points +=   "</tbody>" + 
-            "</table>";
+// Initialize firebase
+let app = firebase.app();
 
+// Create a listener that checks if the user logs in
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    loginSuccessCallback();
+  }
+});
 
-  $('#accordian').append(
-    "<div class='card'>" +
-        "<div class='card-header' id='" + deviceHash + "'>" +
-          "<h5 class='mb-0'>" +
-            "<button class='btn btn-link' data-toggle='collapse' data-target='#collapse" + deviceHash + "' aria-expanded='false' aria-controls='collapse" + deviceHash + "'>" +
-              deviceHash +
-            "</button>" +
-          "</h5>" +
-        "</div>" +
-
-        "<div id='collapse" + deviceHash + "' class='collapse' aria-labelledby='headingOne' data-parent='#accordion'>" +
-          "<div class='card-body'>" +
-            points +
-          "</div>" +
-        "</div>" +
-      "</div>"
-  );
-}
 
 function initMap() {
+  // Make a new map
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: VANCOUVER,
+  });
+}
+
+function formSubmitHandler()
+{
+  let email = $('#emailForm').val();
+  let password = $('#passwordForm').val();
+
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    $('#errorDisplay').text(error.message);
+  });
+}
+
+function loginSuccessCallback() {
+  $('#accordian').empty();
+
   let lines = [];
   let devices = [];
 
-  let app = firebase.app();
+  // Query the database for all the points
   let root = app.database().ref().once('value').then(function(snapshot) {
     let data = snapshot.val();
     devices = Object.keys(data);
@@ -67,13 +61,7 @@ function initMap() {
 
       lines.push(line);
     }
-
-    // Map a new map
-    let map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 13,
-      center: VANCOUVER,
-    });
-
+    
     // For each line
     lines.forEach(function(line, i) {
       // Create path
@@ -93,3 +81,46 @@ function initMap() {
     });
   });
 }
+
+function createCard(deviceHash, line) {
+  // Create the contents of the card
+  let points = "";
+  points += "<table class='table'>" +
+              "<thead>" +
+                "<tr>" +
+                  "<th scope='col'>Latitude</th>" +
+                  "<th scope='col'>Longitude</th>" +
+                "</tr>" +
+              "</thead>" +
+              "<tbody>";
+
+  // Create a table row for each coordinate
+  line.forEach(function(point) {
+    points += "<tr>";
+    points += "<td>" + point.lat + "</td>"
+    points += "<td>" + point.lng + "</td>"
+    points += "</tr>"; 
+  });
+  points +=   "</tbody>" + 
+            "</table>";
+
+  // create the card and append it to the list
+  $('#accordian').append(
+    "<div class='card'>" +
+        "<div class='card-header' id='" + deviceHash + "'>" +
+          "<h5 class='mb-0'>" +
+            "<button class='btn btn-link' data-toggle='collapse' data-target='#collapse" + deviceHash + "' aria-expanded='false' aria-controls='collapse" + deviceHash + "'>" +
+              deviceHash +
+            "</button>" +
+          "</h5>" +
+        "</div>" +
+
+        "<div id='collapse" + deviceHash + "' class='collapse' aria-labelledby='headingOne' data-parent='#accordion'>" +
+          "<div class='card-body'>" +
+            points +
+          "</div>" +
+        "</div>" +
+      "</div>"
+  );
+}
+
